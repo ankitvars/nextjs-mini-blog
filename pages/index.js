4740467e762a7776623bd23bdb3881bcef173495
Ankit Varshney <ankit.varshney@ruptok.com>
2023-09-22 15:41:39 +0530

    initial commit

---
+++ pages/index.js
@@ -0,0 +1,52 @@
+import Head from "next/head";
+import Layout, { siteTitle } from "../components/layout";
+import utilStyles from "../styles/utils.module.css";
+import { getSortedPostsData } from "../lib/posts";
+
+export async function getStaticProps() {
+  const postData = getSortedPostsData();
+  return {
+    props: {
+      postData,
+    },
+  };
+}
+
+export default function Home({postData}) {
+  return (
+    <Layout home>
+      <Head>
+        <title>{siteTitle}</title>
+      </Head>
+      <section className={utilStyles.headingMd}>
+        <p className={utilStyles}>
+          I'm Ankit, a passionate frontend developer at a dynamic fintech
+          startup. I craft user-centric interfaces that empower financial
+          innovation. With a keen eye for design and a knack for cutting-edge
+          technologies, I'm committed to delivering seamless user experiences in
+          the fintech realm.
+        </p>
+        <p>
+          (This is a sample website - you’ll be building a site like this on{" "}
+          <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
+        </p>
+      </section>
+
+      {/* Add this <section> tag below the existing <section> tag */}
+      <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+        <h2 className={`${utilStyles.headingLg}`}>Blog</h2>
+        <ul className={utilStyles.list}>
+          {postData.map(({ id, date, title }) => (
+            <li className={utilStyles.listItem} key={id}>
+              {title}
+              <br />
+              {id}
+              <br />
+              {date}
+            </li>
+          ))}
+        </ul>
+      </section>
+    </Layout>
+  );
+}
