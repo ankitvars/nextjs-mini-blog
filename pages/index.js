3e61b251de297545425247f248deea49934bbb0b
Ankit Varshney <ankit.varshney@ruptok.com>
2023-09-22 16:45:46 +0530

    final commit

--- pages/index.js
+++ pages/index.js
@@ -2,17 +2,19 @@ import Head from "next/head";
 import Layout, { siteTitle } from "../components/layout";
 import utilStyles from "../styles/utils.module.css";
 import { getSortedPostsData } from "../lib/posts";
+import Link from 'next/link';
+import Date from '../components/date';
 
 export async function getStaticProps() {
-  const postData = getSortedPostsData();
+  const allPostData = getSortedPostsData();
   return {
     props: {
-      postData,
+      allPostData,
     },
   };
 }
 
-export default function Home({postData}) {
+export default function Home({allPostData}) {
   return (
     <Layout home>
       <Head>
@@ -36,13 +38,13 @@ export default function Home({postData}) {
       <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
         <h2 className={`${utilStyles.headingLg}`}>Blog</h2>
         <ul className={utilStyles.list}>
-          {postData.map(({ id, date, title }) => (
+          {allPostData.map(({ id, date, title }) => (
             <li className={utilStyles.listItem} key={id}>
-              {title}
+              <Link href={`/posts/${id}`}>{title}</Link>
               <br />
-              {id}
-              <br />
-              {date}
+              <small className={utilStyles.lightText}>
+                <Date dateString={date} />
+              </small>
             </li>
           ))}
         </ul>
